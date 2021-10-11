2dfc695dd48b80133c28a68250cc3cd7ebe90f81
SweetAlcatel <mrneey@gmail.com>
2021-10-11 16:04:07 +0300

    new action

--- src/components/Breed/breed.js
+++ src/components/Breed/breed.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { likeElement } from '../Actions/actions';
+import { likeElement, deleteElement } from '../Actions/actions';
 import { useSelector, useDispatch } from 'react-redux';
 import Styles from './breed.module.css';
 
 export const Breed = ({ id, name, temperament, description }) => {
 
     const dispatch = useDispatch();
-    const data = useSelector((state) => state.data);
-    const likedData = useSelector(state => state.likedData);
-
-    return (
-        <li className={Styles.catAndFacts}>
-            <h1>{name}</h1> 
-            <span>{temperament}</span>
-            <p>{description}</p>
-            <button id={id} className={Styles.actionLike} onClick={() => dispatch(likeElement(id))}>Like</button>
-        </li>
-    );
+    
+        return (
+            <li className={Styles.catAndFacts}>
+                <h1>{name}</h1> 
+                <span>{temperament}</span>
+                <p>{description}</p>
+                <button id={id} className={Styles.actionLike} onClick={() => dispatch(likeElement(id))}>Like</button>
+                <button id={id} className={Styles.actionDelete} onClick={() => dispatch(deleteElement(id))}>Delete</button>
+            </li>
+        );
 };
 
 
